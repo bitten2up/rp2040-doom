0d5dd7054a8af0a86e668629a388e4e879c2e92c
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2024-01-13 00:41:13 +0200

    WAD address

--- up.sh
+++ up.sh
@@ -2,4 +2,4 @@
 xxd -i tiny.whd | sed "s/unsigned/const unsigned/g" >src/tiny.whd.h
 #cp tiny.wad.meta.h src/
 echo dont forget
-echo picotool load -v -n -t bin tiny.whd -o 0x10040000
+echo picotool load -v -n -t bin tiny.whd -o 0x10060000
